186363b1e881310edb8449341adac078ff93660e
donaldsonjulia <julia.donaldson@gmail.com>
2017-03-15 19:42:44 -0700

    nav component styling updates

--- app/components/navigation/navigation.component.js
+++ app/components/navigation/navigation.component.js
@@ -1,7 +1,6 @@
 angular
     .module('portfolio')
     .component('jdNav', {
-      bindings: {},
       templateUrl: './app/components/navigation/navigation.view.html',
       controller: NavCtrl
     });
@@ -10,11 +9,10 @@ angular
 
         this.open = function() {
           document.getElementById("js-nav").style.width = "250px";
-          document.getElementById("js-content").style.marginLeft = "250px";
  };
 
         this.close = function() {
           document.getElementById("js-nav").style.width = "0";
-          document.getElementById("js-content").style.marginLeft = "0";
  };
+
     }
---
+++ app/components/navigation/navigation.service.js
@@ -0,0 +1,13 @@
+// angular.module('portfolio').service('navService', navService);
+//
+// function navService() {
+//
+//   this.openNav = function() {
+//     document.getElementById("js-nav").style.width = "250px";
+// };
+//
+//   this.closeNav = function() {
+//     document.getElementById("js-nav").style.width = "0";
+// };
+//
+// }
