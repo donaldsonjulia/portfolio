1ca60e9a86e7fe43983b8860882c91174ede207e
donaldsonjulia <julia.donaldson@gmail.com>
2017-03-28 15:08:00 -0700

    completed contact form component and functionality using formspree

--- app/components/contact/contact.component.js
+++ app/components/contact/contact.component.js
@@ -7,14 +7,58 @@ angular
 
 function ContactController($http){
 
+    var contactCtrl = this;
+
+    this.formMessage = null;
+
+    this.formError = false;
+
+    this.spinnerActive = false;
+
     this.formData = {
     };
+
+
 
     this.testClick = function() {
       formData = this.formData;
       console.log(formData);
+      messageSent();
     };
 
+    function messageSent() {
+      contactCtrl.spinnerActive = false;
+      contactCtrl.formData = {};
+      contactCtrl.contactForm.$setPristine();
+      contactCtrl.formMessage = "Message sent! Thanks for reaching out!";
+    }
+
+    function messageError() {
+      contactCtrl.spinnerActive = false;
+      contactCtrl.formError = true;
+      contactCtrl.formMessage = "Oops! There was an error sending your message!";
+    }
+
+
+
+    this.submitForm = function() {
+      contactCtrl.spinnerActive = true;
+
+      $http({
+        method: 'POST',
+        url: 'https://formspree.io/julia.donaldson@gmail.com',
+        data: {
+          name: this.formData.name,
+          _replyto: this.formData.replyTo,
+          message: this.formData.message,
+          _subject: 'New message from portfolio site!'
+        }
+      }).then(function successCallback(response) {
+            messageSent();
+      }, function errorCallback(response) {
+            messageError();
+      });
+    };
 
 
 }
