d9dd1db635d81463fa4b97b1d6efe7e0b8e0202e
donaldsonjulia <julia.donaldson@gmail.com>
2017-04-20 17:17:59 -0700

    updates to social media component and about component

--- app/components/about/about.component.js
+++ app/components/about/about.component.js
@@ -6,5 +6,5 @@ angular
     });
 
 function AboutController(){
-    this.bio = "Hi! I'm Julia Donaldson, and I'm a web/fashion/graphic designer and developer. After 12 years in New York City working within the fashion industry, I recently relocated to the Los Angeles area to pursue my interest in web development and design. I love red lipstick, blue jeans, and mint ice cream.";
+
 }
