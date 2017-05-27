fc85b9db6787db29ec353bca7c136702c2741349
donaldsonjulia <julia.donaldson@gmail.com>
2017-05-27 14:55:37 -0700

    removed additional app states

--- app/app.states.js
+++ app/app.states.js
@@ -7,28 +7,5 @@ angular
               nav: {},
               content: {component: 'jdHome'}
             }
-        }).state('work', {
-            url: '/work',
-            views: {
-              nav: { component: 'jdNav' },
-              content: { component: 'jdWork' }
-            },
-            resolve: {
-                work: function(workService) {
-                    return workService.getWork();
-                }
-            }
-        }).state('about', {
-            url: '/about',
-            views: {
-              nav: {component: 'jdNav'},
-              content: {component: 'jdAbout'}
-            }
-        }).state('contact', {
-            url:'/contact',
-            views: {
-              nav: {component: 'jdNav'},
-              content: {component: 'jdContact'}
-            }
         });
     });
